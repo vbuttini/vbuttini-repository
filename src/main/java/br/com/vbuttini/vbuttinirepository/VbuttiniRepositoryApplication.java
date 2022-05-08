cd461c47234368f80efc5095cc863c47eee3e17a
vbuttini-devel <vinicius.buttini@develcode.com.br>
2022-05-08 16:57:34 -0300

    Spring boot inicial config

---
+++ src/main/java/br/com/vbuttini/vbuttinirepository/VbuttiniRepositoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.vbuttini.vbuttinirepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbuttiniRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbuttiniRepositoryApplication.class, args);
+	}
+
+}
